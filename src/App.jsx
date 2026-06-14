708a2e78c8cdc2590e9aeac9a7d4c25015859f8f
masoomeh <gozalimasoomeh.a@gmail.com>
2026-06-15 01:24:04 +0430

    exercise 3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ export default function App() {
   return ( 
     <> 
     {/* <TamrinOne/>  */}
-    <TamrinTwo/>  
-    {/* <TamrinThree/>  */}
+    {/* <TamrinTwo/>   */}
+    <TamrinThree/> 
 
     </> 
   ); 
