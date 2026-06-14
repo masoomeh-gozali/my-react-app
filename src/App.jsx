f6e8da3e7ced54f29b5057c6fb89f4a7b842c429
masoomeh <gozalimasoomeh.a@gmail.com>
2026-06-14 09:56:48 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import Start from "./Start"; 
+// function App() { 
+//   return <Start/>; 
+// } 
+// export default App;  
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind Works!
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Start.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"; 
+function Student(props) { 
+    return <li>{props.name} </li> 
+}
+
+
+export default function Start(){    
+
+
+    return ( 
+        <> 
+        </>
+
+    )  
+
+
+
+
+
+    
+} 
+
+
+
+
+
