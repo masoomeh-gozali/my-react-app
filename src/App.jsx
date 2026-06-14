26da13b55c82caaac682b0ee7a9551a2ff55911d
masoomeh <gozalimasoomeh.a@gmail.com>
2026-06-14 23:04:55 +0430

    exercise 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,33 @@
 //   return <Start/>; 
 // } 
 // export default App;  
-function App() {
-  return (
-    <div className="p-10">
-      <h1 className="text-5xl font-bold text-red-500">
-        Tailwind Works!
-      </h1>
-    </div>
-  )
-}
+export default function App() {
+  return ( 
+    <> 
+    <Student firstName = "susan"  age = "777"/> 
+    </> 
+  ); 
+} 
+function Student(props) { 
+  return ( 
+    <> 
+    <h5> {props.firstName} - {props.age} years old </h5> 
+    </>
 
-export default App
+  ); 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Start.jsx
+++ src/Start.jsx
@@ -1,18 +1,11 @@
 import { useEffect, useState } from "react"; 
-function Student(props) { 
-    return <li>{props.name} </li> 
-}
-
-
 export default function Start(){    
-
-
+    
     return ( 
         <> 
         </>
 
     )  
-
 
 
 
